093229039cc6bb1476ae7814c20fd0d31bed323e
Aldar <aldarkonov@mail.ru>
2022-02-18 19:48:02 +0300

    feat: finish js task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const arr = [];
+  let result = String(Math.abs(n)).split('');
+
+  for (let i = 0; i < result.length; i++) {
+      arr.unshift(result[i]);
+  }
+
+  return arr.join('');
 }
